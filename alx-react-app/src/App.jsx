276ab9a997e6287c7963020462a5708f4cee5a7e
sewalewsetotaw <sewalews29@gmail.com>
2024-11-03 12:22:48 +0300

    Create Specific Components in a React Application

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,7 +1,16 @@
 import WelcomeMessage from "./components/WelcomeMessage";
+import Header from "./components/Header";
+import MainContent from "./components/MainContent";
+import Footer from "./components/Footer";
+
 function App() {
   return ( 
-      <WelcomeMessage />
+      <>
+        <WelcomeMessage />
+        <Header />
+        <MainContent />
+        <Footer />
+      </>
   );
 }
 
