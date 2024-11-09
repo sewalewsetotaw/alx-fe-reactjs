d6e2a4776e7e8de00fc256fa28498b4ca7e6a9a0
sewalewsetotaw <sewalews29@gmail.com>
2024-11-10 01:47:08 +0300

    fixing the error

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -7,6 +7,7 @@ import ProfilePage from "./components/ProfilePage";
 import UserContext from "./components/UserContext";
 function App() {
   const userData = { name: "Jane Doe", email: "jane.doe@example.com" };
+  // const profile={name:"Alice", age:"25" ,bio="Loves hiking and photography"}
   return (
     <>
       <WelcomeMessage />
--- alx-react-app-props/src/components/UserProfile.jsx
+++ alx-react-app-props/src/components/UserProfile.jsx
@@ -1,11 +1,13 @@
+import React, { useContext } from "react";
+import UserContext from "./UserContext";
 const UserProfile = (props) => {
-    return (
-      <div>
-        <h2>{props.name}</h2>
-        <p>Age: {props.age}</p>
-        <p>Bio: {props.bio}</p>
-      </div>
-    );
-  };
+  return (
+    <div>
+      <h2>{props.name}</h2>
+      <p>Age: {props.age}</p>
+      <p>Bio: {props.bio}</p>
+    </div>
+  );
+};
 
 export default UserProfile;
