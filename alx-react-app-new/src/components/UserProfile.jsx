638e3695d4d269250257dd91a8d2f78bf610b982
sewalewsetotaw <sewalews29@gmail.com>
2024-11-09 23:30:41 +0300

    adding some additional styles

--- alx-react-app-new/src/components/UserProfile.jsx
+++ alx-react-app-new/src/components/UserProfile.jsx
@@ -1,9 +1,11 @@
 const UserProfile = (props) => {
   return (
-    <div style={{ border: "1px solid gray", fontSize: "15px", margin: "10px" }}>
-      <h2 style={{color:"blue"}}>{props.name}</h2>
-      <p style={{fontWeight:"bold"}}>Age: {props.age}</p>
-      <p style={{color:"burlywood"}}>Bio: {props.bio}</p>
+    <div style={{ border: "1px solid gray", padding: "10px", margin: "10px" }}>
+      <h2 style={{ color: "blue" }}>{props.name}</h2>
+      <p>
+        Age: <span style={{ fontWeight: "bold" }}>{props.age}</span>
+      </p>
+      <p style={{ color: "burlywood" }}>Bio: {props.bio}</p>
     </div>
   );
 };
