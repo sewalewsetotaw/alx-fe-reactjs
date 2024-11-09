c3318a3185ce533e9f860650077e08b8bc23df32
sewalewsetotaw <sewalews29@gmail.com>
2024-11-10 01:23:15 +0300

    Refactor Prop Drilling to Use Context API

---
+++ alx-react-app-props/src/App.jsx
@@ -0,0 +1,24 @@
+import WelcomeMessage from "./components/WelcomeMessage";
+import Header from "./components/Header";
+import MainContent from "./components/MainContent";
+import Footer from "./components/Footer";
+import UserProfile from "./components/UserProfile";
+import ProfilePage from "./components/ProfilePage";
+import UserContext from "./components/UserContext";
+function App() {
+  const userData = { name: "Jane Doe", email: "jane.doe@example.com" };
+  return (
+    <>
+      <WelcomeMessage />
+      <Header />
+      <MainContent />
+      <Footer />
+      <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
+      <UserContext.Provider value={userData}>
+        <ProfilePage userData={userData} />
+      </UserContext.Provider>
+    </>
+  );
+}
+
+export default App;
