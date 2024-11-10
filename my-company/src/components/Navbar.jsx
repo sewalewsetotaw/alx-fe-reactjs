8ba964ed617855aa6fa6b9b553c252e1aa21e2ea
sewalewsetotaw <sewalews29@gmail.com>
2024-11-10 21:37:15 +0300

    adding display and justify content to CSS

--- my-company/src/components/Navbar.jsx
+++ my-company/src/components/Navbar.jsx
@@ -1,7 +1,15 @@
 import { Link } from "react-router-dom";
 function Navbar() {
   return (
-    <nav style={{ backgroundColor: "#333", color: "white", padding: "5px" }}>
+    <nav
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        backgroundColor: "#333",
+        color: "white",
+        padding: "5px",
+      }}
+    >
       <Link to="/" style={{ marginRight: "10px" }}>
         Home
       </Link>
