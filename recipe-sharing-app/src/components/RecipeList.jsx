73e47a9c7ccaa32440cf6f83d4473a793abb4c29
sewalewsetotaw <sewalews29@gmail.com>
2024-11-16 23:31:40 +0300

    adding store and components

---
+++ recipe-sharing-app/src/components/RecipeList.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import AddRecipeForm from "./AddRecipeForm";
+import useRecipeStore from "../stores/useRecipeStore";
+
+const RecipeList = () => {
+  const recipes = useRecipeStore((state) => state.recipes);
+
+  return (
+    <div>
+      {recipes.map((item) => (
+        <div key={item.id}>
+          <h3>{item.title}</h3>
+          <h3>{item.description}</h3>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default RecipeList;
