cea75e854e4374e09531172485e29a14497f649a
sewalewsetotaw <sewalews29@gmail.com>
2024-11-24 17:57:55 +0300

    Adding nest routes

--- react-router-advanced/src/App.jsx
+++ react-router-advanced/src/App.jsx
@@ -2,8 +2,7 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Profile from "./components/Profile";
-import BlogPost from "./components/BlogPost";
-
+import ProtectedRoute from "./components/ProtectedRoute";
 function App() {
   return (
     <Router>
@@ -16,7 +15,9 @@ function App() {
 
         <Routes>
           <Route path="/" element={<div>Home Page</div>} />
-          <Route path="/profile/*" element={<Profile />} />
+          <Route path="/profile" element={<ProtectedRoute />}>
+            <Route index element={<Profile />} />
+          </Route>
           {/* Dynamic route for blog posts */}
           <Route path="/blog/:id" element={<BlogPost />} />
         </Routes>
