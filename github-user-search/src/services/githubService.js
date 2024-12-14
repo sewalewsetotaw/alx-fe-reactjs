1e7ed8700e0bea76eddb7d6b0cfaf303823949f6
sewalewsetotaw <sewalews29@gmail.com>
2024-12-15 01:12:47 +0300

    adjusting the query

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "https://api.github.com/search/users";
+// const BASE_URL = "https://api.github.com/search/users";
 
 /**
  * Fetch user data from GitHub API with advanced search criteria
@@ -10,14 +10,15 @@ const BASE_URL = "https://api.github.com/search/users";
 export const fetchUserData = async ({ username, location, minRepos }) => {
   try {
     // Construct query parameters
-    let q = [];
-    if (username) q.push(`${username} in:login`);
-    if (location) q.push(`location:${location}`);
-    if (minRepos) q.push(`repos:>=${minRepos}`);
-    const queryString = q.join("+");
+    let query = [];
+    if (username) query.push(`${username} in:login`);
+    if (location) query.push(`location:${location}`);
+    if (minRepos) query.push(`repos:>=${minRepos}`);
+    const queryString = query.join("+");
 
     // Make the API call with the constructed query
-    const url = `${BASE_URL}?q=${queryString}`;
+    // const url = `${BASE_URL}?q=${queryString}`;
+    const url = `https://api.github.com/search/users?q=${queryString}`;
     const response = await axios.get(url);
     return response.data.items; // Return the list of matched users
   } catch (error) {
